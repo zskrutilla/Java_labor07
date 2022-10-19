c2930f3cdbae007a21c9d2b8adc9d97a718ee906
KRUTILLAZS <KRUTILLAZS@NB-22-01-00338.dfad.duf.hu>
2022-10-19 14:21:28 +0200

    Projekt létrehozása Signed-off-by:KRUTILLAZS <KRUTILLAZS@NB-22-01-00338.dfad.duf.hu>

---
+++ src/main/java/uniduna/hu/labor07/FirmWare/CoreFunctions.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uniduna.hu.labor07.FirmWare;
+
+import uniduna.hu.labor07.Beallitas;
+
+/**
+ *
+ * @author KRUTILLAZS
+ */
+public class CoreFunctions {
+
+    public static void kilep() {
+        System.exit(0);
+    }
+
+    public static void beallitasFrame() {
+        new Beallitas().setVisible(true);
+    }
+}
